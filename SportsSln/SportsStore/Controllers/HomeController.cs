78eb985b3eb511c432fa4185123321fd5c218dee
Michael <michaelkaz@outlook.com>
2023-06-05 18:11:27 -0400

    Added more paging tools

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -5,12 +5,16 @@ namespace SportsStore.Controllers;
 
 public class HomeController : Controller
 {
+    public int PageSize = 4;
+    
     private IStoreRepository repository;
 
     public HomeController(IStoreRepository repository)
-    {
-        this.repository = repository;
-    }
+        => this.repository = repository;
 
-    public IActionResult Index() => View(repository.Products);
+    public ViewResult Index(int productPage = 1)
+        => View(repository.Products
+        .OrderBy(p => p.ProductID)
+        .Skip((productPage - 1) * PageSize)
+        .Take(PageSize));
 }
